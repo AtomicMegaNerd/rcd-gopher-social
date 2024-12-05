ae26181b78741f8b8084d0a06f4cb6b7091afe03
Chris Dunphy <chris@megaparsec.ca>
2024-12-05 10:43:38 -0700

    Add a nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description =
+    "This is a sample app that is part of the Backend Engineering with Go course on Udemy";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          # The packages we need for this project
+          buildInputs = with pkgs; [
+            go_1_23
+            go-tools
+            gopls
+            golangci-lint
+            go-task
+            air
+            rainfrog
+          ];
+        };
+      });
+}
