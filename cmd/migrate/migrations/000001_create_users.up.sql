c4f735c5c15761d9a8def02d19ce8a8e5456d703
Chris Dunphy <chris.dunphy@fglsports.com>
2024-12-10 15:55:38 -0700

    Fixes to database schema

--- cmd/migrate/migrations/000001_create_users.up.sql
+++ cmd/migrate/migrations/000001_create_users.up.sql
@@ -1,10 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE IF NOT EXISTS users(
-	id BIGSERIAL PRIMARY KEY,
-	email CITEXT UNIQUE NOT NULL, -- CITEXT is case-insensitive text
-	username VARCHAR(255) UNIQUE NOT NULL,
-	password BYTEA NOT NULL, -- BYTEA is binary data
-	created_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW()
+	id bigserial PRIMARY KEY,
+	email citext UNIQUE NOT NULL, -- citext is case-insensitive text
+	username varchar(255) UNIQUE NOT NULL,
+	password bytea NOT NULL, -- bytea is binary data
+	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
-
